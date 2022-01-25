7bf63f703f6cb3ba176ea26ff999fc076c52cd62
qdtrl <quentindtouroul@gmail.com>
2022-01-25 23:57:17 +0100

    .

--- statblock.h
+++ statblock.h
@@ -3,5 +3,12 @@
 struct StatBlock {
   stattype Strength;
   stattype Intellect;
-  StatBlock() { Strength = (stattype)1u; Intellect = (stattype)1u; }
+  StatBlock() {
+    Strength = (stattype)1u;
+    Intellect = (stattype)1u; 
+  }
+  StatBlock(stattype s, stattype i) { 
+    Strength = s; 
+    Intellect = i; 
+  }
 };
