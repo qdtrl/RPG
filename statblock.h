eec89e239b390948de7f9aad8ecd2ebcb3274502
qdtrl <quentindtouroul@gmail.com>
2022-01-25 23:54:42 +0100

    Part 2

---
+++ statblock.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "stattypes.h"
+struct StatBlock {
+  stattype Strength;
+  stattype Intellect;
+  StatBlock() { Strength = (stattype)1u; Intellect = (stattype)1u; }
+};
--- warrior.h
+++ warrior.h
@@ -1,9 +1,11 @@
 #pragma once
 #include "hp.h"
+#include"statblock.h"
 
-class Warrior : public hp {
+class Warrior : public hp, public {
   public:
-    static const hptype HPGROWTH = 19u;
-    Warrior() : hp(HPGROWTH, HPGROWTH) {}
+    static const hptype HPGROWTH = (hptype)19u;
+    static const stattype BASESTR =  (stattype)10u;
+    Warrior() : hp(HPGROWTH, HPGROWTH) : {}
   private:
 }
